38244a49d767c0b4d8afe7ecb1af1a3edf5293c7
vipin sharma <vipin.sharma502@gmail.com>
2022-03-08 08:18:10 +0530

    chapter 1 getting started with spring.

---
+++ src/test/java/com/example/tacos/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    public HomeControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to ...")));
+    }
+}
--- src/test/java/com/example/tacos/TacoCloudApplicationTests.java
+++ src/test/java/com/example/tacos/TacoCloudApplicationTests.java
@@ -1,8 +1,15 @@
 package com.example.tacos;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/*
+ @SpringBootTest tells JUnit to bootstrap the test with spring boot capabilities
+*/
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TacoCloudApplicationTests {
 
